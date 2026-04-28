36698ee2845599d743247e44134e22d878d0325e
Adrian Bruno <adrianbruno@gmail.com>
2026-04-28 10:34:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'carrusel-ab-v2.2';
+const VERSION = 'carrusel-ab-v2.3';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate', e => {e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)))));self.clients.claim();});
